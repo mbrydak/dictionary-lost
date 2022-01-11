cb8daf36651fc8f9750e18fe5b0689ef62dfc985
Wojciech Brydak <57046745+wbrydak@users.noreply.github.com>
2022-01-11 15:00:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # Dictionary generator using initial words and substitution characters.
 # Creates a dictionary of all possible words and all possible combination of words, preserving initial order of words.
 # Most useful with passwords created following xkcd password-creation tips: https://xkcd.com/936/
-# by: @wbrydak
+# by: Wojciech Brydak (https://github.com/wbrydak)
 # date: 2022-01-11
 # version: 1.0
 # license: MIT
