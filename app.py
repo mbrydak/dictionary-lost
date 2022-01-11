8977aa75de409f82937a7eafaf57425a5a3d83d2
wojtek <brydakwojciech@gmail.com>
2022-01-11 14:54:52 +0100

    comment formatting issues

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-# Dictionary generator using initial words and substitution characters. Creates a dictionary of all possible words and all possible combination of words, preserving initial order of words.
+# Dictionary generator using initial words and substitution characters.
+# Creates a dictionary of all possible words and all possible combination of words, preserving initial order of words.
 # Most useful with passwords created following xkcd password-creation tips: https://xkcd.com/936/
 # by: @wbrydak
 # date: 2022-01-11
